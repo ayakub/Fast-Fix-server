69e8dfcb31fdcd38294093815c75771169753e39
ayakub <ayakub1086@gmail.com>
2022-11-08 22:46:54 +0600

    added service router

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ const { MongoClient, ServerApiVersion, Db } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express()
+
+// middleware
 app.use(cors())
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@sickcluster.nqy80.mongodb.net/?retryWrites=true&w=majority`;
 
-
-const uri = "mongodb+srv://<username>:<password>@sickcluster.nqy80.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const userCollection = client.db('practice-crud').collection('users')
+
 
 async function run() {
+    try {
+        const serviceCollection = client.db('mobileRepair').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
 
 }
-run().catch(err => { console.log(err) })
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
