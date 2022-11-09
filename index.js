c7b69ecdedfca68e095190166c6b5e78cbc27d55
ayakub <ayakub1086@gmail.com>
2022-11-09 16:56:36 +0600

    create service review

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, Db } = require('mongodb');
+const { ObjectID } = require('bson');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express()
@@ -18,7 +19,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const serviceCollection = client.db('mobileRepair').collection('services')
+        const serviceCollection = client.db('mobileRepair').collection('services');
+        const reviewCollection = client.db('mobileRepair').collection('reviews')
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -26,12 +28,42 @@ async function run() {
             const result = await cursor.limit(3).toArray();
             res.send(result)
         })
+
         app.get('/services/all', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
+        app.get('/services/all/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+
+        })
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.service) {
+                query = {
+                    service: req.query.service
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
+
+
+
 
     }
     finally {
